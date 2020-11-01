85c28a195376a542bfb43cea205126db2234f08c
Shani Kumar <52395212+Shani0045@users.noreply.github.com>
2020-11-01 20:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,57 +12,51 @@ def short_link():
     try:
         x=request.form['a']
         link=request.form['b']
-        
-        
-        if request.form['btn']=='url':
-            
-            if link=="nullpointer":
-                s=Shortener()
-                q=s.nullpointer.short(x)
-                return render_template("index.html",c=q)
-            elif link=="tinyurl":
-                s=Shortener()
-                q=s.tinyurl.short(x)
-                return render_template("index.html",c=q)
-            elif link=="dagd":
-                s=Shortener()
-                q=s.dagd.short(x)
-                return render_template("index.html",c=q)
-            elif link=="isgd":
-                s=Shortener()
-                q=s.isgd.short(x)
-                return render_template("index.html",c=q)
-            elif link=="chilpit":
-                s=Shortener()
-                q=s.chilpit.short(x)
-                return render_template("index.html",c=q)
-            elif link=="osdb":
-                s=Shortener()
-                q=s.osdb.short(x)
-                return render_template("index.html",c=q)
-            elif link=="qpsru":
-                s=Shortener()
-                q=s.qpsru.short(x)
-                return render_template("index.html",c=q)
-            elif link=="bitly":
-                s=Shortener(api_key="5b0366c4f79b0b7d1b1ca6e9aaf89c57fb0b115e")
-                q=s.bitly.short(x)
-                return render_template("index.html",c=q)
+        if link=="nullpointer":
+            s=Shortener()
+            q=s.nullpointer.short(x)
+            return render_template("index.html",c=q)
+        elif link=="tinyurl":
+            s=Shortener()
+            q=s.tinyurl.short(x)
+            return render_template("index.html",c=q)
+        elif link=="dagd":
+            s=Shortener()
+            q=s.dagd.short(x)
+            return render_template("index.html",c=q)
+        elif link=="isgd":
+            s=Shortener()
+            q=s.isgd.short(x)
+            return render_template("index.html",c=q)
+        elif link=="chilpit":
+            s=Shortener()
+            q=s.chilpit.short(x)
+            return render_template("index.html",c=q)
+        elif link=="osdb":
+            s=Shortener()
+            q=s.osdb.short(x)
+            return render_template("index.html",c=q)
+        elif link=="qpsru":
+            s=Shortener()
+            q=s.qpsru.short(x)
+            return render_template("index.html",c=q)
+        elif link=="bitly":
+            s=Shortener(api_key="5b0366c4f79b0b7d1b1ca6e9aaf89c57fb0b115e")
+            q=s.bitly.short(x)
+            return render_template("index.html",c=q)
 
-            elif link=="none":
-                return render_template("index.html")
+        elif link=="none":
+            return render_template("index.html")
                      
     except:
         return render_template("index.html")
 
 @app.route("/copy",methods=["POST","GET"])
 def copy():
-    if request.method=="POST":
-        tx=request.form['txt']
-        pc.copy(tx)
-        t=pc.paste()
-        return render_template("index.html",c="url copy")
-    return render_template("index.html",c="not copied")
+    tx=request.form['txt']
+    pc.copy(tx)
+    t=pc.paste()
+    return render_template("index.html",c="url copy")
     
 if __name__=="__main__":
     app.run()
