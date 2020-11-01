700b7290f1d2d8270ee59ed1d5d36be712a53d00
Shani Kumar <52395212+Shani0045@users.noreply.github.com>
2020-11-01 10:50:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import*
+from pyshorteners import Shortener
+import clipboard as clip
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/short_link",methods=["GET","POST"])
+def short_link():
+    try:
+        x=request.form['a']
+        link=request.form['b']
+        txt2=request.form['txt']
+        
+        if request.form['btn']=='url':
+            
+            if link=="nullpointer":
+                s=Shortener()
+                q=s.nullpointer.short(x)
+                return render_template("index.html",c=q)
+            elif link=="tinyurl":
+                s=Shortener()
+                q=s.tinyurl.short(x)
+                return render_template("index.html",c=q)
+            elif link=="dagd":
+                s=Shortener()
+                q=s.dagd.short(x)
+                return render_template("index.html",c=q)
+            elif link=="isgd":
+                s=Shortener()
+                q=s.isgd.short(x)
+                return render_template("index.html",c=q)
+            elif link=="chilpit":
+                s=Shortener()
+                q=s.chilpit.short(x)
+                return render_template("index.html",c=q)
+            elif link=="osdb":
+                s=Shortener()
+                q=s.osdb.short(x)
+                return render_template("index.html",c=q)
+            elif link=="qpsru":
+                s=Shortener()
+                q=s.qpsru.short(x)
+                return render_template("index.html",c=q)
+            elif link=="bitly":
+                s=Shortener(api_key="5b0366c4f79b0b7d1b1ca6e9aaf89c57fb0b115e")
+                q=s.bitly.short(x)
+                return render_template("index.html",c=q)
+
+            elif link=="none":
+                return render_template("index.html")
+            
+        elif request.form['btn']=='copy':
+            a=clip.copy(txt2)
+            return render_template("index.html",c="URL copied")
+            
+    except:
+        return render_template("index.html")
+    
+if ___name__=="__main__":
+    app.run()
+
